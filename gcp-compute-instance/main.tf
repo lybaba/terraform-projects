cbb191b71003b5dd142fa1c0eb38d6b737be77b8
Babaly LY <lybaba@gmail.com>
2024-11-01 17:31:48 +0100

    fix teraform configuration file

--- gcp-compute-instance/main.tf
+++ gcp-compute-instance/main.tf
@@ -1,3 +1,36 @@
+terraform {
+
+  required_providers {
+
+    google = {
+
+      source = "hashicorp/google"
+
+    }
+
+  }
+
+}
+
+
+
+provider "google" {
+
+
+
+  project = "qwiklabs-gcp-01-3d4159637420"
+
+  region = "us-east1"
+
+  zone = "us-east1-c"
+
+}
+
+resource "google_compute_instance" "terraform" {
+
+  name = "terraform"
+
+  machine_type = "e2-medium"
 
   tags = ["web", "dev"]
 
@@ -15,8 +48,8 @@
     network = "default"
     access_config {
     }
-}
+  }
 
-allow_stopping_for_update = true
+  allow_stopping_for_update = true
 
 }
