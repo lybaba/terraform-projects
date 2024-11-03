c1792c32fa000694b23b15df614b91ebfd9c5ae0
Babaly LY <lybaba@gmail.com>
2024-11-03 16:48:25 +0100

    gcs revert remote backend

--- remote-backend/main.tf
+++ remote-backend/main.tf
@@ -1,15 +1,15 @@
 provider "google" {
-  project     = "qwiklabs-gcp-01-8d7da656cd9d"
-  region      = "us-west1"
+  project = "qwiklabs-gcp-01-8d7da656cd9d"
+  region  = "us-west1"
 }
 resource "google_storage_bucket" "test-bucket-for-state" {
-  name        = "qwiklabs-gcp-01-8d7da656cd9d"
-  location    = "US" # Replace with EU for Europe region
+  name                        = "qwiklabs-gcp-01-8d7da656cd9d"
+  location                    = "US" # Replace with EU for Europe region
   uniform_bucket_level_access = true
+  force_destroy               = true
 }
 terraform {
-  backend "gcs" {
-    bucket  = "qwiklabs-gcp-01-8d7da656cd9d"
-    prefix  = "terraform/state"
+  backend "local" {
+    path = "terraform/state/terraform.tfstate"
   }
 }
